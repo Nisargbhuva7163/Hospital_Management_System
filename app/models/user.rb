69bdd6599e803eba5e2224c643da6cfa9ca7317e
nisarg-bhuva <nisarg1895@gmail.com>
2025-05-23 14:40:50 +0530

    Feat(signup): sign up with organization and user data

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  rolify
+  belongs_to :organization
+  accepts_nested_attributes_for :organization
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable , :confirmable
+end
